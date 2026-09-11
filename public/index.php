c40e0e8396747e7f1b042157bbfbe1bf363d2039
Zeina <engxeinamalek@gmail.com>
2026-09-11 19:29:38 +0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,69 @@
+<?php
+header('Content-Type: application/json');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/Database.php';
+
+$routes = require __DIR__ . '/../routes/api.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$route = null;
+$params = [];
+
+foreach ($routes[$method] ?? [] as $routePattern => $handler) {
+
+    if (strpos($routePattern, '{id}') !== false) {
+
+        $pattern = str_replace(
+            '{id}',
+            '([0-9]+)',
+            $routePattern
+        );
+
+        $pattern = '#^' . $pattern . '$#';
+
+        if (preg_match($pattern, $uri, $matches)) {
+            $route = $handler;
+            $params['id'] = $matches[1];
+            break;
+        }
+
+    } else {
+
+        // Route عادي بدون parameters
+        if ($routePattern === $uri) {
+            $route = $handler;
+            break;
+        }
+    }
+}
+
+if ($route) {
+
+    [$class, $methodName] = explode('@', $route);
+
+    if (class_exists($class)) {
+
+        $object = new $class($db);
+
+        $response = empty($params)
+            ? $object->$methodName()
+            : $object->$methodName($params['id']);
+
+        http_response_code($response['status']);
+
+        echo json_encode($response);
+
+        exit();
+    }
+}
+http_response_code(404);
+
+echo json_encode([
+    'message' => 'Route not found'
+]);
